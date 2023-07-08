bf819aae99f39b6075ff7fa5f03888df11a61bba
vani9340 <sg5428702@gmail.com>
2023-07-08 15:56:49 +0530

    c

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,17 +2,17 @@ function Form() {
     return (
       <div className="bg-light_grn border-t border-light_grn rounded-t-full mt-36">
         {/* 1st container */}
-        <div className="bg-light_grn">
+        <div className="bg-light_grn flex flex-col items-center pt-24 pb-16 gap-8">
           <div className="bg-light_grn">
-            <h1 className="bg-light_grn">Ready to get started?</h1>
-            <p className="bg-light_grn">Create your free account, no credit card needed.</p>
+            <h1 className="bg-light_grn text-5xl font-normal text-grn">Ready to get started?</h1>
+            <p className="bg-light_grn text-lg pt-8 text-center">Create your free account, no credit card needed.</p>
           </div>
           <button className="px-5 py-3 bg-grn text-white rounded-xl text-xl">Get Conversa</button>
         </div>
         {/* 2nd container */}
-        <div class='bg-grn'>
-        <div class='bg-grn'>
-            <h1 class='bg-grn'>Get in Touch</h1>
+        <div class='bg-grn '>
+        <div class='bg-grn flex flex-col items-center'>
+            <h1 class='bg-grn '>Get in Touch</h1>
             <textarea placeholder="Write your message*"/>
             {/* input field */}
             <div class='bg-grn'>
